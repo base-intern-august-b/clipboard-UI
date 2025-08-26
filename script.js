421b71088402081eae71fa797bc97021356801f3
Tennessine_699 <d.hirano123@gmail.com>
2025-08-26 15:22:12 +0900

    fix: bad asyncronus api calling

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 let mes_length = 0;
 
 function get_channel_num(){
-    fetch("/channels") // リクエストを送信
-        .then((response) => response.json())
-        .then((data) => {
-            return(data.length);
-    });
+    return 3;
 }
 
 function sendmess(inputmes) {
@@ -23,46 +19,38 @@ function sendmess(inputmes) {
     mes_length += 1;
 }
 
-function get_channel_name(){
-    const channel_list = [];
-    fetch("/channels") // リクエストを送信
-        .then((response) => response.json())
-        .then((data) => {
-            for(let i=0;i<data.length; i++)
-                channel_list.push(data[i].channel_name);
-    });
-    return channel_list;
+async function get_channel_name() {
+    const response = await fetch("/api/v1/channels");
+    const data = await response.json();
+    return data.map(item => item.channel_name);
 }
 
-function get_channel_id(channel_id){
-    fetch("/channels") // リクエストを送信
-        .then((response) => response.json())
-        .then((data) => {
-            return data[channel_id].channel_id
-    });
+async function get_channel_id(channel_id) {
+    const response = await fetch("/api/v1/channels");
+    const data = await response.json();
+    return data[channel_id].channel_id;
 }
 
-function get_messgae(channel_id){
+async function get_messgae(channel_id){
+    const response = await fetch(`/api/v1/channels/${channel_id}/messages`) // リクエストを送信
+    const data = await response.json();
     const mes_data=[];
-    fetch(`/channels/${channel_id}/messages`) // リクエストを送信
-        .then((response) => response.json())
-        .then((data) => {
-            for(let i=0;i<data.length; i++)
-                mes_data.push(data[i].nessage);
-    });
+    for(let i=0;i<data.length; i++)
+        mes_data.push(data[i].nessage);
     return mes_data;
 }
 
 function channel_list(num) {
-    const channel_list = get_channel_name()
-    for (i = 0; i < num; i++) {
-        let tag = document.createElement("div");
-        tag.innerHTML = channel_list[i];
-        tag.id(i);
-        tag.setAttribute("onclick", "display_mes_clear(),display_mes(this.innerHTML,this.id)");
-        const parent = document.getElementById("channel_list");
-        parent.appendChild(tag);
-    }
+    get_channel_name().then(channel_list => {
+        for (i = 0; i < num; i++) {
+            let tag = document.createElement("div");
+            tag.innerHTML = channel_list[i];
+            tag.id(i);
+            tag.setAttribute("onclick", "display_mes_clear(),display_mes(this.innerHTML,this.id)");
+            const parent = document.getElementById("channel_list");
+            parent.appendChild(tag);
+        }
+    });
 }
 
 function make_channel(channel_name) { 
@@ -102,40 +90,42 @@ function display_mes(channel_title, channel_id) {
     tag.innerHTML = channel_title;
     parent.appendChild(tag);
     
-    const mes_data = get_messgae(get_channel_id(channel_id));
-
-    for (let i = 1; i < mes_data+1; i++) {
-        tag = document.createElement("div");
-        tag.classList.add("messagebox");
-        tag.id = i;
-        parent = document.getElementById("messages");
-        parent.appendChild(tag);
-
-        tag = document.createElement("div");
-        tag.classList.add("message_title");
-        tag.id = -i;
-        parent = document.getElementById(i);
-        parent.appendChild(tag);
-
-        tag = document.createElement("div");
-        tag.classList.add("message_info");
-        tag.innerHTML = "username - time";
-        parent = document.getElementById(-i);
-        parent.appendChild(tag);
-
-        tag = document.createElement("span");
-        tag.classList.add("ping_span");
-        tag.innerHTML = "✖︎";
-        parent = document.getElementById(-i);
-        parent.appendChild(tag);
-
-        tag = document.createElement("div");
-        tag.classList.add("message_text");
-        tag.innerHTML = mes_data[i-1];
-        parent = document.getElementById(i);
-        parent.appendChild(tag);
-        mes_length += 1;
-    }
+    get_channel_id(channel_id).then(channel_id => {
+        get_messgae(channel_id).then(mes_data => {
+            for (let i = 1; i < mes_data+1; i++) {
+                tag = document.createElement("div");
+                tag.classList.add("messagebox");
+                tag.id = i;
+                parent = document.getElementById("messages");
+                parent.appendChild(tag);
+        
+                tag = document.createElement("div");
+                tag.classList.add("message_title");
+                tag.id = -i;
+                parent = document.getElementById(i);
+                parent.appendChild(tag);
+        
+                tag = document.createElement("div");
+                tag.classList.add("message_info");
+                tag.innerHTML = "username - time";
+                parent = document.getElementById(-i);
+                parent.appendChild(tag);
+        
+                tag = document.createElement("span");
+                tag.classList.add("ping_span");
+                tag.innerHTML = "✖︎";
+                parent = document.getElementById(-i);
+                parent.appendChild(tag);
+        
+                tag = document.createElement("div");
+                tag.classList.add("message_text");
+                tag.innerHTML = mes_data[i-1];
+                parent = document.getElementById(i);
+                parent.appendChild(tag);
+                mes_length += 1;
+            }
+        })
+    })
 }
 
 function clipmes(){
